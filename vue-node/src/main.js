032e51b54ac9fdb069b1f516ad436e5230756105
mestokaya1830 <123832169+mestokaya1830@users.noreply.github.com>
2023-08-09 03:08:16 -0400

    bgb

--- vue-node/src/main.js
+++ vue-node/src/main.js
@@ -1,10 +1,8 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
 import router from './router'
-import store from './store'
-import './style.css'
 
 createApp(App)
 .use(router)
-.use(store)
 .mount('#app')
