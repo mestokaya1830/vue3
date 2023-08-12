3e9e672b6c75f4584d081fdd7b45fe8fd50552b0
mestokaya1830 <123832169+mestokaya1830@users.noreply.github.com>
2023-08-12 05:02:51 -0400

    vfv

---
+++ components/5-component-events/src/components/Popup.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h2>These are Component Click Events</h2>
+    <input type="text" v-model="message" placeholder="Enter the message" />
+    <input type="button" @click="$emit('messageEvent', message)" value="Message Event Parameter" />
+    <input type="button" @click="$emit('calcEvent', 20)" value="Calc Event With Parameter" />
+    <input type="button" @click="$emit('closeEvent')" value="Close Event" />
+  </div>
+</template>
+<script>
+  export default {
+    name: "PopupComponent",
+    emits:{
+      messageEvent(message){
+        if(!message){
+          return false
+        }
+        return true
+      }
+    },
+    data() {
+      return { 
+        message:''
+      }
+    }
+  };
+  </script>
+  <style scoped>
+    div{
+      width: 400px;
+      padding: 10px;
+      margin-top: 20px;
+      text-align: center;
+      border-radius: 4px;
+      border: 1px solid #ccc;
+      box-shadow: 1px 1px 10px #999;
+    }
+    button{
+      margin: 20px;
+      padding: 10px 20px;
+    }
+  </style>
+<style scoped>
+input{
+  width: 100%;
+  margin: 10px 0;
+  min-height: 30px;
+}
+input[type="text"]{
+  padding-left: 10px;
+  border: none;
+  border: 1px solid #ddd;
+}
+input[type="text"]:focus{
+  border-radius: 3px;
+  outline: none;
+  box-shadow:inset 2px 2px 5px #444;
+}
+</style>
--- components/5-component-events/src/views/HomeView.vue
+++ components/5-component-events/src/views/HomeView.vue
@@ -1,24 +1,57 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <input type="button" value="Open Message Component" @click="handle=true">
-    <msg-component v-if="handle" @close="handle=false"/>
+    <input type="button" value="Open Popup Component" @click="handle=true">
+    <p>Custom  click event (close) and (message) comes from popup component</p>
+    <p>You can click this event and do what do you want</p>
+    
+    <Popup v-if="handle"
+    @messageEvent="messageEvent($event)"
+    @calcEvent="calcEvent($event)"
+    @closeEvent="closeEvent()"
+    />
+    
+    <p>Message state: {{ message }}</p>
+    <p>Calc state: {{ calc }}</p>
+    <p>Handle state: {{ handle }}</p>
   </div>
 </template>
 
 <script>
-import MsgComponent from '../components/MsgComponent.vue'
+import Popup from '../components/Popup.vue'
 
 export default {
   name:'HomePage',
   components:{
-    'msg-component' : MsgComponent
+    Popup
   },
   data(){
     return {
       title:'Home Page',
-      handle:false
+      handle:false,
+      message:'',
+      calc: 0,
+    }
+  },
+  methods:{
+    messageEvent(event){
+      this.message = event
+    },
+    calcEvent(event){
+      this.calc = event * 20
+    },
+    inputEvent(event){
+      this.name = event
+    },
+    closeEvent(){
+      this.handle = false
     }
   }
 }
 </script>
+
+<style scoped>
+p{
+  margin: 10px 0;
+}
+</style>
