53bb2e10fcef999bb9d32449a26e05407e1c321c
mestokaya1830 <123832169+mestokaya1830@users.noreply.github.com>
2023-08-16 17:26:39 -0400

    xsxs

---
+++ z-vue-example/pagination/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Navbar />
+    <RouterView id="master-page" />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/NavBar.vue";
+export default{
+  name:'AppPage',
+  components:{Navbar},
+  data() {
+    return{}
+  }
+}
+</script>
+<style>
+ul {
+  display: flex;
+  list-style: none;
+}
+ul > li {
+  margin: 0 15px;
+}
+#master-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
+
+
---
+++ z-vue-example/pagination/src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/" class="links">Home</router-link>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+  nav{
+    display: flex;
+  }
+  .links{
+    margin: 10px;
+  }
+  .links{
+    text-decoration: none;
+  }
+  .active{
+    color: green;
+  }
+</style>
