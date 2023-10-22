54335e5ddae0d5a140a30768eb36d069a7f8665d
mestokaya <mesto1830@outlook.com>
2023-10-22 22:26:00 +0200

    uu

---
+++ composition-api/src/components/EmitComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Emit Component Model</h1>
+    <button @click="hideMe('hideModel')">Hide Me!</button>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+const modelHandel = ref(false)
+const hideMe = defineEmits(['hideModel'])
+
+</script>
--- composition-api/src/views/Component.vue
+++ composition-api/src/views/Component.vue
@@ -3,8 +3,12 @@
     <h2>{{ title }}</h2>
     <PropsComponent :message="message"/>
     <SlotComponent>
-        <h2>Here comes from parent as slot</h2>
+      <h2>Here comes from parent as slot</h2>
     </SlotComponent>
+    <button @click="showModel = true">Show Model</button>
+    <div>
+      <EmitComponent v-if="showModel" @hideModel="showModel=false"/>
+    </div>
   </div>
 </template>
 
@@ -12,9 +16,12 @@
 import {ref, onMounted} from 'vue'
 import PropsComponent from '../components/PropsComponent.vue'
 import SlotComponent from '../components/SlotComponent.vue'
+import EmitComponent from '../components/EmitComponent.vue'
 
  const title = ref('Component')
  const message = ref('Component Props')
+
+ const showModel = ref(false)
 
  onMounted(() => {
   console.log(title)
