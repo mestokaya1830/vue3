a714ad37573d26a03e30c9c6076e32d4c0f4deba
mestokaya <mesto1830@outlook.com>
2023-10-24 00:58:54 +0200

    gggg

--- composition-api/src/views/Fetch.vue
+++ composition-api/src/views/Fetch.vue
@@ -2,11 +2,18 @@
   <div>
     <h2>{{ title }}</h2>
 
-    <template v-for="item in users" :key="item.id">
+    <!-- <template v-for="item in users" :key="item.id">
       <div>
         <span>{{ item.id }}</span>
         <span>{{ item.name }}</span>
         <span>{{ item.age }}</span>
+      </div>
+    </template> -->
+
+    <template v-for="item in photos" :key="item.id">
+      <div>
+        <span>{{ item.id }}</span>
+        <a :href="item.url" target="_blank" >{{ item.url }}</a>
       </div>
     </template>
   </div>
@@ -14,13 +21,21 @@
 
 <script setup>
 import {ref, onMounted} from 'vue'
-
+import axios from 'axios'
  const title = ref('Fetch')
-const users = ref([
-  {id:1, name:'Mesto', age: 50},
-  {id:2, name:'Ali', age: 40},
-  {id:3, name:'Deniz', age: 30}
-])
+  // const users = ref([
+  //   {id:1, name:'Mesto', age: 50},
+  //   {id:2, name:'Ali', age: 40},
+  //   {id:3, name:'Deniz', age: 30}
+  // ])
+  let photos = ref(null)
+  fetch('https://jsonplaceholder.typicode.com/photos')
+  .then(data => data.json())
+  .then(data => photos.value = data)
+
+  // axios.get('https://jsonplaceholder.typicode.com/photos').then(result => {
+  //   photos.value = result.data
+  // })
 
  onMounted(() => {
   console.log(title)
