ca071207fa25c28d891745ca876ca0d03214d03b
mestokaya <mesto1830@outlook.com>
2023-11-03 18:25:38 +0100

    vvv

--- vue-socket.io/src/socket.js
+++ vue-socket.io/src/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client" //npm i socket.io-client
-export const socket = io('http://localhost:3000') //connect backend
+export const socket = io('http://localhost:3000',{
+  transports: ["websocket"]//for cluster mode
+}) //connect backend
 
 socket.on("connect", () => {
   console.log('New user connected...')
